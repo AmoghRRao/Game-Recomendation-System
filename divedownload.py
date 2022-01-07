37636c36097db753358cada5904f85f719d56e29
Amogh Rao <amogh.r.rao@gmail.com>
2022-01-07 16:06:20 +0530

    to download pkl file

---
+++ divedownload.py
@@ -0,0 +1,6 @@
+from google_drive_downloader import GoogleDriveDownloader as gdd
+
+
+gdd.download_file_from_google_drive(file_id='14LXxwldPTV5K1EmrDr-HQc1svBhMKDvT',
+                                    dest_path='./similarity.pkl',
+                                    unzip=False)
